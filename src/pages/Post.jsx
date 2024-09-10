21f9f0e78462904a92b70080ee297a245a1b3d0b
Abhay Ratnakar <a.s.ratnakar2192@gmail.com>
2024-09-11 00:23:12 +0530

    final commit

---
+++ src/pages/Post.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import { Button, Container } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
+
+export default function Post() {
+    const [post, setPost] = useState(null);
+    const { slug } = useParams();
+    const navigate = useNavigate();
+
+    const userData = useSelector((state) => state.auth.userData);
+
+    const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug).then((post) => {
+                if (post) setPost(post);
+                else navigate("/");
+            });
+        } else navigate("/");
+    }, [slug, navigate]);
+
+    const deletePost = () => {
+        appwriteService.deletePost(post.$id).then((status) => {
+            if (status) {
+                appwriteService.deleteFile(post.featuredImage);
+                navigate("/");
+            }
+        });
+    };
+
+    return post ? (
+        <div className="py-8 ">
+            <Container className="flex items-center justify-center">
+                <div className="bg-white pb-1 w-96 rounded-xl h-66 py-1 flex flex-col items-center overflow-hidden">
+                <div className="w-[90%] flex h-[70%] justify-center mb-4 relative rounded-xl p-2">
+                    <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-xl"
+                    />
+
+                    {isAuthor && (
+                        <div className="absolute right-6 top-6">
+                            <Link to={`/edit-post/${post.$id}`}>
+                                <Button bgColor="bg-green-500" className="mr-2 px-3 py-1">
+                                    Edit
+                                </Button>
+                            </Link>
+                            <Button bgColor="bg-red-500" className="px-3 py-1" onClick={deletePost}>
+                                Delete
+                            </Button>
+                        </div>
+                    )}
+                </div>
+                <div className="w-64 mb-6">
+                    <h1 className="text-2xl font-bold">{post.title}</h1>
+                </div>
+                <div className="browser-css">
+                    {parse(post.content)}
+                </div>
+                </div>
+            </Container>
+        </div>
+    ) : null;
+}
